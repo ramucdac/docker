58426e3ee3d9620fda7dba437fe7f7e92d901faf
ramucdac <ramucdac@gmail.com>
2018-07-05 14:16:44 +0530

    commiting all changes

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -6,5 +6,5 @@ sudo docker build -t mycentos:httpdv1.0 .
 sudo mkdir /data
 sudo docker stop web
 sudo docker rm web
-########
+#########
 sudo docker run -td -p 80:80 -v /data:/var/www/html --name=web mycentos:httpdv1.0
